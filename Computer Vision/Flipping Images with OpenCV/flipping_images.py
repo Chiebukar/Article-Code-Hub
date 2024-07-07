e6333705280351af3da545ce0694ab2fba6e92c5
Ozechi Samuel <sam23chuks@gmail.com>
2024-07-07 10:20:21 +0100

    Update flipping_images.py

--- Computer Vision/Flipping Images with OpenCV/flipping_images.py
+++ Computer Vision/Flipping Images with OpenCV/flipping_images.py
@@ -1,20 +1,20 @@
 # import OpenCV
 import cv2
 
-# # load an display the original image
+# # load and display the original image
 image = cv2.imread('logo.jpg')
-# cv2.imshow("Original", image)
-# cv2.waitKey(0)
+cv2.imshow("Original", image)
+cv2.waitKey(0)
 
-# # flip the image horizontally
-# flipped = cv2.flip(image, 1)
-# cv2.imshow("Flipped Horizontally", flipped)
-# cv2.waitKey(0)
+# flip the image horizontally
+flipped = cv2.flip(image, 1)
+cv2.imshow("Flipped Horizontally", flipped)
+cv2.waitKey(0)
 
-# # flip the image vertically
-# flipped = cv2.flip(image, 0)
-# cv2.imshow("Flipped Vertically", flipped)
-# cv2.waitKey(0)
+# flip the image vertically
+flipped = cv2.flip(image, 0)
+cv2.imshow("Flipped Vertically", flipped)
+cv2.waitKey(0)
 
 # flip the image along both axes
 flipped = cv2.flip(image, -1)
